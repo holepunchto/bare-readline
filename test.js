d72c99387fea76ed744833ad02c1d33f1644c522
Sean Zellmer <sean@lejeunerenard.com>
2024-05-14 14:03:34 -0500

    Add support for \n as end of line inputs which trigger `line` event
    The `\r\n` test is skipped as it doesn't pass yet.

--- test.js
+++ test.js
@@ -20,3 +20,41 @@ test('basic', (t) => {
   input.write('hello world')
   input.write('\r')
 })
+
+test('supports linefeed as line event', (t) => {
+  t.plan(2)
+
+  const input = new PassThrough()
+  const rl = new Readline({ input })
+
+  rl
+    .on('line', (line) => {
+      t.is(line, 'hello world')
+      rl.close()
+    })
+    .on('close', () => {
+      t.pass('closed')
+    })
+
+  input.write('hello world')
+  input.write('\n')
+})
+
+test.skip('supports \\r\\n as single line event', (t) => {
+  t.plan(2)
+
+  const input = new PassThrough()
+  const rl = new Readline({ input })
+
+  rl
+    .on('line', (line) => {
+      t.is(line, 'hello world')
+      rl.close()
+    })
+    .on('close', () => {
+      t.pass('closed')
+    })
+
+  input.write('hello world')
+  input.write('\r\n')
+})
